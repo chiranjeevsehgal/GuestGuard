855902b06ba3a876de1fa6ef5d976a7aa51b4857
chiranjeevsehgal <chiranjeevsehgal@gmail.com>
2024-02-13 22:45:48 +0530

    FAQ Updated

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -12,16 +12,16 @@ function Faq() {
                     <p className="mt-4 mb-8 text-gray-600">This section serves as a repository of answers to frequently encountered inquiries, providing comprehensive solutions to common concerns.</p>
                     <div className="space-y-4">
                         <details className="w-full border rounded-lg">
-                            <summary className="px-4 py-6 focus:outline-none focus-visible:ri">Ex orci laoreet egestas sapien magna egestas scelerisque?</summary>
-                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
+                            <summary className="px-4 py-6 focus:outline-none focus-visible:ri">Question 1?</summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">Answer 1. </p>
                         </details>
                         <details className="w-full border rounded-lg">
-                            <summary className="px-4 py-6 focus:outline-none focus-visible:ri">Lorem at arcu rutrum viverra metus sapien venenatis lobortis odio?</summary>
-                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
+                            <summary className="px-4 py-6 focus:outline-none focus-visible:ri">Question 2?</summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">Answer 2. </p>
                         </details>
                         <details className="w-full border rounded-lg">
-                            <summary className="px-4 py-6 focus:outline-none focus-visible:ri">Eleifend feugiat sollicitudin laoreet adipiscing bibendum suscipit erat?</summary>
-                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+                            <summary className="px-4 py-6 focus:outline-none focus-visible:ri">Question 3?</summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">Answer 3. </p>
                         </details>
                     </div>
                 </div>
